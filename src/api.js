b978256f5738ef307a39ec8eb1447149d68c99d4
Tirth <tirthsolanki29923@gmail.com>
2023-01-10 23:47:58 +0530

    add weather api key

--- src/api.js
+++ src/api.js
@@ -9,5 +9,5 @@ export const GEO_API_URL =
   "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "";
+export const WEATHER_API_KEY = "a3711f49c64da09e67c7ae02529a366f";
 
--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -1,6 +1,6 @@
 const CurrentWeather = ({data}) => {
   return (
-    <div className="weather w-72 border-spacing-2 text-white bg-slate-600 mr-20 mt-5 mb-auto ml-0">
+    <div className="weather w-80 border-spacing-2 rounded-xl text-white bg-slate-600 mr-20 mt-5 mb-auto ml-0">
       <div className="top flex justify-between items-center">
         <div>
           <p className="city font-semibold text-lg m-0 ">{data.city}</p>
@@ -10,28 +10,28 @@ const CurrentWeather = ({data}) => {
 
       </div>
       <div className="bottom flex justify-between items-center">
-        <p className="temp font-semibold text-6xl w-auto mr-2">{Math.round(data.main.temp)}°C</p>
+        <p className="temp font-semibold text-6xl w-auto mr-2">{Math.round(data.main.temp)-273}°C</p>
         <div className="details w-[100%] pl-5">
         <div className="parameter-row flex justify-between">
             <span className="par-l text-left font-normal text-xs">Details</span>
         </div>
         <div className="parameter-row flex justify-between">
             <span className="par-l text-left font-normal text-xs">Feels like</span>
-            <span className="par-v text-right font-semibold text-xs">{Math.round(data.main.feels_like)}°C</span>
+            <span className="par-v text-right font-semibold text-xs pr-7">{Math.round(data.main.feels_like)-273}°C</span>
         </div>
         <div className="parameter-row flex justify-between">
             <span className="par-l text-left font-normal text-xs">Wind</span>
-            <span className="par-v text-right font-semibold text-xs">{data.wind.speed} m/s</span>
+            <span className="par-v text-right font-semibold text-xs pr-7">{data.wind.speed} m/s</span>
         </div>
         <div className="parameter-row flex justify-between">
             <span className="par-l text-left font-normal text-xs">Humidity</span>
-            <span className="par-v text-right font-semibold text-xs">{data.main.humidity}%</span>
+            <span className="par-v text-right font-semibold text-xs pr-7">{data.main.humidity}%</span>
         </div>
         <div className="parameter-row flex justify-between">
             <span className="par-l text-left font-normal text-xs">Pressure</span>
-            <span className="par-v text-right font-semibold text-xs">{data.main.pressure} hPa </span>
+            <span className="par-v text-right font-semibold text-xs pr-7">{data.main.pressure} hPa </span>
         </div>
-
+        
         </div>
       </div>
     </div>
